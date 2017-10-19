2b1965090e87feb61d6c8696b10dd87a2f8ca4ce
Valeria del Rio <valeriadlrio@gmail.com>
2017-10-19 11:41:46 -0300

    Subiendo a firebase las imagenes

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -1,3 +1,4 @@
+import { by } from 'protractor';
 import { FileItem } from './../models/file-item';
 import { Injectable } from '@angular/core';
 // import { AngularFire, FirebaseListObservable,   } from 'angularfire2';
@@ -21,6 +22,22 @@ export class CargaImagenesService {
 
   cargar_imagenes_firebase (archivos: FileItem[]) {
     console.log(archivos);
+    const storageRef = firebase.storage().ref();
+    // tslint:disable-next-line:prefer-const
+    for (let item of archivos) {
+      item.estaSubiendo = true;
+      const uploadTask: firebase.storage.UploadTask =
+                storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`).put(item.archivo);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot) => item.progreso = ( uploadTask.snapshot.bytesTransferred / uploadTask.snapshot.totalBytes  ) * 100,
+        (error) => console.error('Error al subir', error),
+        () => {
+          item.url = uploadTask.snapshot.downloadURL;
+          item.estaSubiendo = false;
+          this.guardarImagen({nombre: item.nombreArchivo, url: item.url});
+        }
+      );
+    }
   }
 
   private guardarImagen( imagen: any) {
